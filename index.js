a21d97f7bee0a738f1b3354f7ff092ecb4bdc6c1
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2024-04-27 12:31:36 +0100

    Advertise builds id earlier

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const core   = require('@actions/core');
+const core = require('@actions/core');
 const semver = require('semver');
+
+let available = [
+    "5.8",
+    "5.10", "5.12", "5.14", "5.16", "5.18",
+    "5.20", "5.22", "5.24", "5.26", "5.28",
+    "5.30", "5.32", "5.34", "5.36", "5.38",
+    "devel",
+];
 
 try {
     const since_perl = semver.coerce(core.getInput('since-perl'));
     const with_devel = core.getInput('with-devel') == "true";
-
-    let available = [
-        "5.8",
-        "5.10", "5.12", "5.14", "5.16", "5.18",
-        "5.20", "5.22", "5.24", "5.26", "5.28",
-        "5.30", "5.32", "5.34", "5.36", "5.38",
-        "devel",
-    ];
 
     let filtered = available.filter(
         (item) => {
