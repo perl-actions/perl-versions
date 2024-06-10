95427f421367e640b6a7f0d2e3ae07f6b11cd9a9
Graham Knop <haarg@haarg.org>
2024-06-10 08:21:00 +0200

    normalize whitespace
    The index.js file was using 4 spaces for indents, so update .editorconfig
    to match that. And update the other js/json files to match as well.

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ const pluginJs = require("@eslint/js");
 
 
 module.exports = [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+    {languageOptions: { globals: globals.node }},
+    pluginJs.configs.recommended,
 ];
